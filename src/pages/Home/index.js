15a187df8ee9d247626e9b7cf8d761ce1fe01f42
William <william.guitarrari@dartdigital.com.br>
2021-04-17 18:12:45 -0300

    Add carousel

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,48 +1,20 @@
 import React from 'react';
+import 'react-multi-carousel/lib/styles.css';
 
 import Newsletter from '../../components/Newsletter';
 import CustomSlider from '../../components/Banner';
+import Carousel from '../../components/Carousel';
 
-import { Container, BannerSobre, Sliders } from './styles';
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
-import Slider from 'infinite-react-carousel';
+import { Container } from './styles';
 
 
 
 function Home() {
-  const responsive = {
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 3,
-      slidesToSlide: 3 // optional, default to 1.
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 2,
-      slidesToSlide: 2 // optional, default to 1.
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1,
-      slidesToSlide: 1 // optional, default to 1.
-    }
-  };
+
   return (
     <Container>
       <CustomSlider />
-      <div>
-        <Carousel
-          responsive={responsive}
-          infinite={true}
-          customTransition="all .5"
-        >
-          <div>Item 1</div>
-          <div>Item 2</div>
-          <div>Item 3</div>
-          <div>Item 4</div>
-        </Carousel>;
-      </div>
+      <Carousel />
       <Newsletter />
     </Container>
   );
